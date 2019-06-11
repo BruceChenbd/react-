d59456442778ae7fc43a92466fa10c84aeb1fedd
陈宝东 <chenbaodong@chenbaodongdeMacBook-Pro.local>
2019-06-11 09:00:24 +0800

    提交top组件

--- personal-manage/src/components/top/top.js
+++ personal-manage/src/components/top/top.js
@@ -1,17 +1,29 @@
 import React from 'react';
-
+import { Icon } from 'antd'
 
 class Top extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-    
+            collapsed:true
         }
+    }
+    toggle = () => {
+        this.setState({
+            collapsed: !this.state.collapsed,
+        });
+        this.props.toggleClick(this.state.collapsed)
     }
     render () {
         return (
             <div className="header">
-               hhah
+               <Icon
+                className="trigger"
+                type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+                onClick={this.toggle}
+                />
+
+                我是标题
             </div>
         )
     }   
--- personal-manage/src/container/admin/admin.js
+++ personal-manage/src/container/admin/admin.js
@@ -12,9 +12,10 @@ class Admin extends React.Component{
         }
     }
 
-    toggle = () => {
+    toggle = (value) => {
+        console.log(value)
         this.setState({
-            collapsed: !this.state.collapsed,
+            collapsed: value,
         });
     }
     render () {
@@ -24,15 +25,7 @@ class Admin extends React.Component{
                   <NavLeft></NavLeft>
                </Sider>
                <Layout>
-                    <Header style={{ background: '#fff', padding: 0 }}>
-                            <Icon
-                            className="trigger"
-                            type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
-                            onClick={this.toggle}
-                            />
-                            {/* <Top></Top> */}
-                            <div>hahahhah</div>
-                    </Header>
+                    <Top toggleClick={this.toggle}></Top>
                     <Content  style={{
                             margin: '24px 16px',
                             padding: 24,
